1ca2121e6e6a81cb7d2345705373f160ed52382c
honey4251996 <peacehoney425@gmail.com>
2020-06-24 13:29:48 +0800

    update app.js

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
